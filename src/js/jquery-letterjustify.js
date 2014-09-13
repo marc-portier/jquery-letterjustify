8393d6647146cf0283346df83b652f4a01a3c27b
Marc Portier <marc.portier@gmail.com>
2014-09-13 20:56:03 +0200

    support for <em> and * markers for emphasis

--- src/js/jquery-letterjustify.js
+++ src/js/jquery-letterjustify.js
@@ -66,17 +66,33 @@
     }
     
     LetterJustify.config = {
-        "case"  :  "keep"
-      , "break" :  "smart"
-      , "keep"  :  "basic"
+        "case"  : "keep"
+      , "break" : "smart"
+      , "keep"  : "basic"
+      , "emph"  : "on"
     }
     
+    var BARE = /[*]/g
+      , EMTAGS = /((<em>\s*)|(\s*<\/em>))/g
+      , BASIC = /[^*a-zA-Z0-9]/g
+    ;
     LetterJustify.prototype.apply = function() {
+        
+        // if emph == on we do an effort to maintain <em> info
+        if (this.config.emph == "on") {
+            //we replace em markers with *
+            var org = this.$elm.html();
+            console.log("org ==> ", org);
+            var emptxt = org.replace(EMTAGS, "*").replace("**"," ");;
+            console.log("emptext ==> ", emptxt);
+            this.$elm.html(emptxt);
+        }
+        
         var text = this.$elm.text();
         
         // only keep some chars
         if (this.config.keep == "basic") { 
-            text = text.replace(/[^0-9a-zA-Z ]/, "");
+            text = text.replace(BASIC, "");
         }
         // apply case
         if (this.config.case == "upper") { 
@@ -91,11 +107,14 @@
         } else if (this.config.break == "smart") {
             var xl = 0
               , sm = text.length
-              , words = text.split(" ");
+              , words = text.split(" ")
+              , bares = []
             ;
             
             words.forEach(function(word, i){
-                var size = word.length;
+                var bare = word.replace(BARE,"")
+                bares[i] = bare;
+                var size = bare.length;
                 if (size > xl) { xl = size; }
                 if (size < sm) { sm = size; }
             });
@@ -110,22 +129,29 @@
                 } else {
                     var spi = spans.length -1;
                     var spw = spans[spi];
-                    if ((spw + " " + word).length <= breaksize) {
+                    if ((spw + " " + bares[i]).length <= breaksize) {
                         spans[spi] = spw + " " + word;
                     } else {
                         spans[spi+1] = word;
                     }
                 }
             });
+            
         }
         var html = "<span>" + spans.join("</span><br><span>") + "</span>"; 
+        console.log("spans ==> ", spans);
         this.$elm.html(html);
         $('span', this.$elm).each(LetterJustify.justify);
     }
     LetterJustify.justify = function() {
-        var $this = $(this);
-        var txt = $this.text();
+        var $this = $(this)
+          , txt = $this.text()
+          , emph = false
+          , bare = txt.replace(BARE,"")
+          , markers = 0
+        ;
         
+        $this.text(bare);
         $this.css('display', 'inline-block');
         var txtWidth = $this.get(0).clientWidth;
         var txtHeight = $this.get(0).clientHeight;
@@ -144,12 +170,21 @@
         
         for(var i = 0; i < txt.length; i++)
         {
+            //toggle emphasis on * markers
+            if (txt.charAt(i) == "*") {
+                emph = !emph;
+                markers++;
+                continue;
+            }
             var $ltr = $("<span>" + txt.charAt(i) + "</span>");
             $ltr.css('display', 'inline-block');
             $ltr.css('position', 'absolute');
+            if (emph) {
+                $ltr.css('font-weight', 900);
+            }
             $this.append($ltr);
 
-            var positionRatio = i / (txt.length - 1);
+            var positionRatio = (i-markers) / (bare.length - 1);
             var textWidth = $ltr.get(0).clientWidth;
 
             var indent = 100 * positionRatio;
